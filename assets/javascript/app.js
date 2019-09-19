cb2932b3937a12123e42d93f8f8bce1b90aa037e
Michael Williams <mwilliams2964@gmail.com>
2019-09-18 18:31:18 -0700

    fixed game over function and nonfunctional gifs, then adjusted gif sizes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,42 @@ $(document).ready(function () {
 
     let questions = [
         {
+            q: "The __ richest people in the US have more weath than the rest of the country combined.",
+            a: "3",
+            choices: ["3", "5", "10", "15"],
+            gif: `<div style="width:100%;height:0;padding-bottom:128%;position:relative;"><iframe 
+                src="https://giphy.com/embed/N8BgIThVVDrDG" width="70%" height="65%" 
+                style="position:absolute" frameBorder="0" class="giphy-embed" 
+                allowFullScreen></iframe></div><p><a 
+                href="https://giphy.com/gifs/N8BgIThVVDrDG">via GIPHY</a></p>`
+        },
+        {
             q: "What country produces the highest quality drinking water?",
             a: "USA",
             choices: ["USA", "Norway", "Sweden", "Greenland"],
             gif: `<iframe src="https://giphy.com/embed/5ME9j9hbSJYrK" width="480" height="358" 
-                frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a 
-                href="https://giphy.com/gifs/stephen-colbert-gif-gifs-5ME9j9hbSJYrK">via GIPHY</a></p>`
+            frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a 
+            href="https://giphy.com/gifs/stephen-colbert-gif-gifs-5ME9j9hbSJYrK">via GIPHY</a></p>`
+        },
+        {
+            q: "Which of the following countries has the highest taxes?",
+            a: "USA",
+            choices: ["Mexico", "Ireland", "Korea", "USA"],
+            gif: `<div style="width:100%;height:0;padding-bottom:80%;position:relative;"><iframe 
+                src="https://giphy.com/embed/sIuhhl4qp5OY8" width="70%" height="70%" 
+                style="position:absolute" frameBorder="0" class="giphy-embed" 
+                allowFullScreen></iframe></div><p><a 
+                href="https://giphy.com/gifs/dancing-pay-taxes-sIuhhl4qp5OY8">via GIPHY</a></p>`
+        },
+        {
+            q: "According to the CPI, the North Korea ranks ___ among the MOST corrupt countries.",
+            a: "4th",
+            choices: ["1st", "2nd", "3rd", "4th"],
+            gif: `<div style="width:100%;height:0;padding-bottom:67%;position:relative;"><iframe 
+            src="https://giphy.com/embed/kmtu15lTKZEas" width="80%" height="80%" 
+            style="position:absolute" frameBorder="0" class="giphy-embed" 
+            allowFullScreen></iframe></div><p><a 
+            href="https://giphy.com/gifs/basketball-north-rodman-kmtu15lTKZEas">via GIPHY</a></p>`
         },
         {
             q: "Which country has contributed most to the world's overall carbon pollution?",
@@ -39,57 +69,36 @@ $(document).ready(function () {
             a: "22nd",
             choices: ["3rd", "9th", "17th", "22nd"],
             gif: `<div style="width:100%;height:0;padding-bottom:59%;position:relative;"><iframe 
-                src="https://giphy.com/embed/l2SpQNlDBMFtkp4Zi" width="100%" height="100%" 
+                src="https://giphy.com/embed/l2SpQNlDBMFtkp4Zi" width="80%" height="80%" 
                 style="position:absolute" frameBorder="0" class="giphy-embed"allowFullScreen></iframe></div><p><a 
                 href="https://giphy.com/gifs/hyperrpg-rabbit-hyper-rpg-valiant-l2SpQNlDBMFtkp4Zi">via GIPHY</a></p>`
         },
-        {
-            q: "According to the CPI, the North Korea ranks ___ among the MOST corrupt countries.",
-            a: "4th",
-            choices: ["1st", "2nd", "3rd", "4th"],
-            gif: `<div style="width:100%;height:0;padding-bottom:64%;position:relative;"><iframe 
-                src="https://giphy.com/embed/11jucxrYlHScko" width="100%" height="100%" 
-                style="position:absolute" frameBorder="0" class="giphy-embed" 
-                allowFullScreen></iframe></div><p><a 
-                href="https://giphy.com/gifs/missile-launching-11jucxrYlHScko">via GIPHY</a></p>`
-        },
-        {
-            q: "The __ richest people in the US have more weath than the rest of the country combined.",
-            a: "3",
-            choices: ["3", "5", "10", "15"],
-            gif: `<div style="width:100%;height:0;padding-bottom:128%;position:relative;"><iframe 
-                src="https://giphy.com/embed/N8BgIThVVDrDG" width="100%" height="100%" 
-                style="position:absolute" frameBorder="0" class="giphy-embed" 
-                allowFullScreen></iframe></div><p><a 
-                href="https://giphy.com/gifs/N8BgIThVVDrDG">via GIPHY</a></p>`
-        },
-        {
-            q: "Which of the following countries has the highest taxes?",
-            a: "USA",
-            choices: ["Mexico", "Ireland", "Korea", "USA"],
-            gif: `<div style="width:100%;height:0;padding-bottom:49%;position:relative;"><iframe 
-                src="https://giphy.com/embed/rE04CIrzIrSN2" width="100%" height="100%" 
-                style="position:absolute" frameBorder="0" class="giphy-embed" 
-                allowFullScreen></iframe></div><p><a 
-                href="https://giphy.com/gifs/taxes-workaholics-adam-demamp-rE04CIrzIrSN2">via 
-                GIPHY</a></p>`
-        }
     ]
-    
+
     // global declarations
-    let wrongGif = `<div style="width:100%;height:0;padding-bottom:91%;position:relative;"><iframe 
-        src="https://giphy.com/embed/t7QimwymlAq4oJiWBH" width="100%" height="100%" 
+    let wrongGif = `<div style="width:100%;height:0;padding-bottom:60%;position:relative;"><iframe 
+        src="https://giphy.com/embed/3oz8xLd9DJq2l2VFtu" width="70%" height="70%" 
         style="position:absolute" frameBorder="0" class="giphy-embed" 
         allowFullScreen></iframe></div><p><a 
-        href="https://giphy.com/gifs/bill-taxes-gates-t7QimwymlAq4oJiWBH">via GIPHY</a></p>`
+        href="https://giphy.com/gifs/election2016-donald-trump-election-2016-3oz8xLd9DJq2l2VFtu">via 
+        GIPHY</a></p>`;
+    let timeIsUp = `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe 
+        src="https://giphy.com/embed/fUwOs80ja3sTPpjndh" width="70%" height="70%" 
+        style="position:absolute" frameBorder="0" class="giphy-embed" 
+        allowFullScreen></iframe></div><p><a 
+        href="https://giphy.com/gifs/netflix-gabriel-iglesias-gabe-mr-fUwOs80ja3sTPpjndh">via 
+        GIPHY</a></p>`
     let time = 7;
+    let numCorrectGuesses = 0;
+    let numIncorrectGuesses = 0;
+    let numTimeOut = 0;
     let questionNum = 0;
     let countdown;
 
 
     // Starts game
-    $("#start").on("click", nextQuestion);
-    
+    $(document).on("click", "#start", nextQuestion);
+
 
     function nextQuestion() {
         $("#timer").empty();
@@ -98,7 +107,7 @@ $(document).ready(function () {
         $("#timer").text("Time Remaining: " + time);
         countdown = setInterval(decrement, 1000);
         appendChoices();
-        console.log(questionNum);
+        // console.log(questionNum);
     }
 
     function decrement() {
@@ -108,21 +117,52 @@ $(document).ready(function () {
             clearInterval(countdown);
             let correctAns = $(`<br><p>Your time is up! The correct answer was ${questions[questionNum].a}</p><br><br>`);
             $("#question").append(correctAns);
+            $("#question").append(timeIsUp);
+            numTimeOut++;
             questionNum++;
-            setTimeout(nextQuestion, 2 * 1000);
+            console.log("decrement function questionNum = " + questionNum);
+            console.log("question.length = " + questions.length)
+            if (questionNum > questions.length) {
+                gameOver();
+            } else {
+                console.log(`a setTimeout function from else statement 
+                    is running because the condition (questionNum > questions.length) was not met`);
+                setTimeout(nextQuestion, 4 * 1000);
+            }
         }
     }
 
     function appendChoices() {
         $("#question").empty();
-        $("#question").append("<br><br>" + questions[questionNum].q + "<br><br>");
-        for (let i = 0; i < 4; i++) {
-            let choice = $(`<p class="question"><a class="start" class="btn btn-primary btn-lg btn-block" href="#" role="button">${questions[questionNum].choices[i]}</a></p>`);
-            choice.attr("data-ans", questions[questionNum].choices[i]);
-            $("#question").append(choice);
+
+        // 
+        if (questionNum == 8) {
+            gameOver();
+        } else {
+            // setTimeout(nextQuestion, 4 * 1000);
+            $("#question").append("<br><br>" + questions[questionNum].q + "<br><br>");
+        }
+        // 
+
+        // $("#question").append("<br><br>" + questions[questionNum].q + "<br><br>");
+        for (let i = 0; i < questions[i].choices.length; i++) {
+            // 
+            if (questionNum == 8) {
+                gameOver();
+            } else {
+                // setTimeout(nextQuestion, 4 * 1000);
+                // $("#question").append("<br><br>" + questions[questionNum].q + "<br><br>");
+                let choice = $(`<p class="question"><a class="start" class="btn btn-primary btn-lg btn-block" href="#" role="button">${questions[questionNum].choices[i]}</a></p>`);
+                choice.attr("data-ans", questions[questionNum].choices[i]);
+                $("#question").append(choice);
+            }
+            // 
+            // let choice = $(`<p class="question"><a class="start" class="btn btn-primary btn-lg btn-block" href="#" role="button">${questions[questionNum].choices[i]}</a></p>`);
+            // choice.attr("data-ans", questions[questionNum].choices[i]);
+            // $("#question").append(choice);
         }
 
-        console.log(questionNum)
+        // console.log(questionNum)
     }
 
     // This handles click events when player answers a question
@@ -133,9 +173,11 @@ $(document).ready(function () {
         if (userGuess === questions[questionNum].a) {
             correct();
             questionNum++;
+            console.log("correct questionNum = " + questionNum)
         } else {
             incorrect();
             questionNum++;
+            console.log("incorrect questionNum = " + questionNum)
         }
     })
 
@@ -145,7 +187,12 @@ $(document).ready(function () {
         let correctAns = $(`<br><p>Correct! The answer was ${questions[questionNum].a}</p><br><br>`);
         $("#question").append(correctAns);
         $("#question").append(questions[questionNum].gif)
-        setTimeout(nextQuestion, 4 * 1000);
+        numCorrectGuesses++;
+        if (questionNum == 8) {
+            gameOver();
+        } else {
+            setTimeout(nextQuestion, 4 * 1000);
+        }
     }
 
     function incorrect() {
@@ -154,22 +201,65 @@ $(document).ready(function () {
             </p><br><br>`);
         $("#question").append(incorrectAns);
         $("#question").append(wrongGif);
-        setTimeout(nextQuestion, 4 * 1000);
+        numIncorrectGuesses++;
+        if (questionNum == 8) {
+            gameOver();
+        } else {
+            setTimeout(nextQuestion, 4 * 1000);
+        }
+        // if (questionNum > questions.length) {
+        //     gameOver();
+        // } else {
+        //     setTimeout(nextQuestion, 4 * 1000);
+        // }
     }
 
     // code this game over function for when there are no more questions left.
     // Be careful where to place this. Probably after questionNum++
     function gameOver() {
 
-        if (questionNum > questions.length) {
-            clearInterval(countdown)
-            $("#question").empty();
-            let correctAns = $(`<br><p>Correct! The answer was ${questions[questionNum].a}
-                </p><br><br>`);
-            $("#question").append(correctAns);
-            setTimeout(nextQuestion, 4 * 1000);
+        clearInterval(countdown)
+        $("#question").empty();
+        let endGameDisplay = $(`<br>
+            <p>Game over!</p><br>
+            <p>You answered ${numCorrectGuesses} questions corectly.</p><br>
+            <p>You answered ${numIncorrectGuesses} questions incorrectly.</p>`);
+        $("#question").append(endGameDisplay);
+        // timeout score function added here
+        addTimeoutScore();
+        $("#question").append("<button class='reset'>Try again?</button>")
+    }
+
+    function addTimeoutScore () {
+        if (numTimeOut > 0) {
+            let timeOutDisplay = $(`<br>
+                <p>You let ${numTimeOut} questions time out. If the time ran out, 
+                that counts as an incorrect guess. </p><br>
+                <p>Your score is 
+                ${100 * (numCorrectGuesses / (numIncorrectGuesses + numTimeOut
+                    + numCorrectGuesses))}%.</p>`);
+            $("#question").append(timeOutDisplay);
+        } else {
+            let scoreDisplayWithNoTimeouts = $(`<br>
+                <p>Your score is ${100 * (numCorrectGuesses / (numIncorrectGuesses + numTimeOut
+                    + numCorrectGuesses))}%.</p>`);
+            $("#question").append(scoreDisplayWithNoTimeouts);
         }
     }
 
-
+    $(document).on('click', '.reset', function() {
+        time = 7;
+        numCorrectGuesses = 0;
+        numIncorrectGuesses = 0;
+        numTimeOut = 0;
+        questionNum = 0;
+        countdown;
+        $("#timer").empty();
+        $("#question").empty();
+        $("#timer").append(`<p class="lead">This is a fact-checking game. 
+            Do you believe fake-news? Get ready to find out!</p>
+            <hr class="my-4"><p>Click the start button to begin the game.</p>
+            <br><p class="lead"><a id="start" class="btn btn-primary btn-lg 
+            btn-block" href="#" role="button">Start</a></p>`)
+    })
 })

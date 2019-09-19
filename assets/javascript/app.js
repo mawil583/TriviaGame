911359b4299bfb839ba8d05ce3f931fddd5335cc
Michael Williams <mwilliams2964@gmail.com>
2019-09-19 16:03:27 -0700

    changed gifs from URL to file download for better formatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,83 +5,55 @@ $(document).ready(function () {
             q: "The __ richest people in the US have more weath than the rest of the country combined.",
             a: "3",
             choices: ["3", "5", "10", "15"],
-            gif: `<img src="./assets/images/BillGates.gif" style="max-width:40%;height:auto;"></div>`
-            // gif: `<div style="width:100%;height:0;padding-bottom:90%;position:relative;"><iframe 
-            //     src="https://giphy.com/embed/N8BgIThVVDrDG" width="40%" height="40%" style="position:absolute;left:30%;" 
-            //     frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`
+            gif: `<img src="./assets/images/BillGates.gif" style="max-width:40%;height:auto;">`
         },
         {
             q: "The US ties for ___ among 9 other nations for drinking water quality.",
             a: "1st",
             choices: ["1st", "2nd", "3rd", "4th"],
-            gif: `<div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe 
-                src="https://giphy.com/embed/5ME9j9hbSJYrK" width="50%" height="50%" style="position:absolute;left:25%" 
-                frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`
+            gif: `<img src="./assets/images/Colbert.gif" style="min-width:50%;max-width:70%;height:auto;">`
         },
         {
             q: "According to the CPI, the North Korea ranks ___ among the MOST corrupt countries (1st being most corrupt).",
             a: "4th",
             choices: ["1st", "2nd", "3rd", "4th"],
-            gif: `<div style="width:100%;height:0;padding-bottom:67%;position:relative;"><iframe 
-                src="https://giphy.com/embed/kmtu15lTKZEas" width="50%" height="50%" 
-                style="position:absolute;left:25%" frameBorder="0" class="giphy-embed" 
-                allowFullScreen></iframe></div>`
+            gif: `<img src="./assets/images/NorthKorea.gif" style="max-width:40%;height:auto;"></img>`
         },
         {
             q: "Which country has historically contributed most to the world's overall carbon pollution (from 1751 to 2017)?",
             a: "USA",
             choices: ["China", "India", "USA", "Saudi Arabia"],
-            gif: `<div style="width:100%;height:0;padding-bottom:53%;position:relative;"><iframe 
-                src="https://giphy.com/embed/l46CfkDr9obp42Uq4" width="50%" height="50%" 
-                style="position:absolute;left:25%" frameBorder="0" class="giphy-embed" 
-                allowFullScreen></iframe></div>`
+            gif: `<img src="./assets/images/LacklusterFlagWaving.gif" style="max-width:40%;height:auto;"></img>`
         },
         {
             q: "Since the year 2000, which country has cut greenhouse emissions by the greatest percentage (not absolute amount)?",
             a: "Denmark",
             choices: ["USA", "Denmark", "France", "United Kingdom"],
-            gif: `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe 
-                src="https://giphy.com/embed/5UxMJrdciUPfAsuUZb" width="50%" height="50%" 
-                style="position:absolute;left:25%" frameBorder="0" class="giphy-embed" 
-                allowFullScreen></iframe></div>`
+            gif: `<img src="./assets/images/Denmark.gif" style="max-width:40%;height:auto;"></img>`
         },
         {
             q: "What percentage of Earth's oxygen does the Amazon Rainforest produce?",
             a: "6-9%",
             choices: ["3-5%", "6-9%", "10-13%", "14-17%"],
-            gif: `<div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe 
-                src="https://giphy.com/embed/WvixhOVEixWZUEH8la" width="50%" height="50%" 
-                style="position:absolute;left:25%" frameBorder="0" class="giphy-embed" 
-                allowFullScreen></iframe></div>`
+            gif: `<img src="./assets/images/Rainforest.gif" style="max-width:40%;height:auto;"></img>`
         },
         {
             q: "Which of the following countries has the highest taxes?",
             a: "USA",
             choices: ["Mexico", "Ireland", "Korea", "USA"],
-            gif: `<div style="width:100%;height:0;padding-bottom:80%;position:relative;"><iframe 
-                src="https://giphy.com/embed/sIuhhl4qp5OY8" width="50%" height="50%" 
-                style="position:absolute;left:25%" frameBorder="0" class="giphy-embed" 
-                allowFullScreen></iframe></div>`
+            gif: `<img src="./assets/images/Taxes.gif" style="max-width:40%;height:auto;"></img>`
         },
         {
             q: "According to the CPI, the US ranks ___ among the LEAST corrupt countries (1st being least corrupt).",
             a: "22nd",
             choices: ["9th", "17th", "22nd", "26th"],
-            gif: `<div style="width:100%;height:0;padding-bottom:59%;position:relative;"<iframe 
-                src="https://giphy.com/embed/l2SpQNlDBMFtkp4Zi" width="50%" height="50%" 
-                style="position:absolute;left:25%" frameBorder="0" class="giphy-embed"allowFullScreen></iframe></div>`
+            gif: `<img src="./assets/images/ItsFine.gif" style="max-width:40%;height:auto;"></img>`
         },
     ]
 
     // global declarations
-    let wrongGif = `<div style="width:100%;height:0;padding-bottom:60%;position:relative;"><iframe 
-        src="https://giphy.com/embed/3oz8xLd9DJq2l2VFtu" width="50%" height="50%" 
-        style="position:absolute;left: 25%;" frameBorder="0" class="giphy-embed" 
-        allowFullScreen></iframe></div>`;
-    let timeIsUp = `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe 
-        src="https://giphy.com/embed/fUwOs80ja3sTPpjndh" width="40%" height="40%" 
-        style="position:absolute;left:30%" frameBorder="0" class="giphy-embed" 
-        allowFullScreen></iframe></div>`
+    let wrongGif = `<img src="./assets/images/Wrong.gif" style="max-width:40%;height:auto;"></img>`;
+    let timeIsUp = `<img src="./assets/images/TimeIsUp.gif" style="max-width:40%;height:auto;"></img>`;
     let time = 20;
     let numCorrectGuesses = 0;
     let numIncorrectGuesses = 0;
@@ -188,7 +160,7 @@ $(document).ready(function () {
         $("#question").append(endGameDisplay);
         // timeout score function added here
         addTimeoutScore();
-        $("#question").append("<br><button class='choices btn btn-primary m-1 w-50'>Try again?</button>")
+        $("#question").append("<br><button class='reset btn btn-primary m-1 w-50'>Try again?</button>")
     }
 
     function addTimeoutScore () {
